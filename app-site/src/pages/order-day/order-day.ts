f1fc246c30e06c5b3f8ec97690f2b2d40bb065e1
wzh700 <511363001@qq.com>
2019-07-14 21:01:09 +0800

    日期选择页面修改 构思完成，明天把功能完善好

--- app-site/src/pages/order-day/order-day.ts
+++ app-site/src/pages/order-day/order-day.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { stringify } from '@angular/core/src/render3/util';
 
 /**
  * Generated class for the OrderDayPage page.
@@ -16,7 +17,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class OrderDayPage {
 
 dayallnum=3;
-showdaynum=1;
+showdaynum=0;
 timebetween=30*60*1000;
 am_num=4;
 am_start=8*60*60*1000;
@@ -29,6 +30,10 @@ orderdaylist=[
   ["1562976000000",
     "1562979600000"]
 ];
+
+showtodaylist={am:[],pm:[]};
+
+
 
 greeting  = "11";
  isDisabled = false;
@@ -44,18 +49,36 @@ greeting  = "11";
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.getTodaylist();
+    
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderDayPage');
   }
 
-//测试效果
-  onClick(event){
-    this.greeting = 'Greeting!!';
-    //console.log(event);
-    console.log(event.type);
-    this.isSpecial=true;
+
+  //获取今天展示的showtodaylist数据
+getTodaylist(){
+//第一生成上午显示数据
+var i=0;
+var am_show_list=[];
+for(i = 0;i<this.am_num;i++) {
+  //这个变量不能放在外面
+  var one_time={showtime:"",isorder:false};
+  //获取某天日期的时间戳
+  let showtime=(new Date(new Date().toLocaleDateString())).valueOf()+this.showdaynum * 24 * 60 * 60 * 1000+this.am_start+i*this.timebetween;
+  one_time.showtime=showtime.toString();  
+  one_time.isorder=false;
+  am_show_list.push(one_time);
   }
+//生成上午的时间点
+this.showtodaylist.am=am_show_list;
+//console.log(this.showtodaylist.am);
+
+}
+
+
 
 }
