67c47352fdeed97534e02a396ba66bef25d0ae1a
wzh700 <511363001@qq.com>
2019-07-21 07:07:26 +0800

    预约查询功能页面(客户)

--- app-site/src/pages/order-me/order-me.ts
+++ app-site/src/pages/order-me/order-me.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the OrderMePage page.
  *
@@ -14,12 +14,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'order-me.html',
 })
 export class OrderMePage {
+  orderlistunuse = [
+    {
+      "thumbimg":"assets/img/thumbnail-totoro.png",
+      "orderpersonname": "张三",
+      "shopname": "店一",
+      "ordertime": "1563638800000",
+      "tips": "备注要求",    
+      "show": false, 
+    },
+    {
+      "thumbimg":"assets/img/thumbnail-totoro.png",
+      "orderpersonname": "李四",
+      "shopname": "店一",
+      "ordertime": "1563638800000",
+      "tips": "备注要求",   
+      "show": false, 
+    }
+  ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderMePage');
   }
+
+  showAlert(ordertime) {
+    console.log(ordertime);
+    console.log((new Date(ordertime)));
+    const alert = this.alertCtrl.create({
+      title: '取消确认',
+      subTitle: ('您确认取消'.concat((new Date(ordertime)).toString())).concat('的预约？'),
+      buttons: ['确定']
+    });
+    alert.present();
+  }
+
 
 }
