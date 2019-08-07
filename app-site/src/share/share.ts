17a66f6c63ff817d20c5c2843b1ef7cc0463bd20
trimono <threemonoliths@gmail.com>
2019-08-08 06:11:20 +0800

    配置后端地址

--- app-site/src/share/share.ts
+++ app-site/src/share/share.ts
@@ -1,12 +1,9 @@
 import { Http, Headers, Response, RequestOptions, ResponseContentType } from '@angular/http';
 
 //本地调试url
-export const baseUrl = "http://localhost:4000/api/v1/"
-// export const baseUrlRaw = "http://localhost:4000/"
+// export const baseUrl = "http://localhost:4000/api/v1/"
+export const baseUrl = "http://112.132.249.108:17001/api/v1/"
 
-//部署url
-// export const baseUrl = "http://112.132.249.108:4000/admin_api/v1/"
-// export const baseUrlRaw = "http://112.132.249.108:4000/"
 
 export const userId = "weixin000001";
 export const openId = "19820325";
